5ca8fcd6c23d00f5449af91e5003e93178a8e1a4
hamdanahnorsipa <hamdanahnorsipa136@gmail.com>
2023-05-31 12:37:51 +0800

    selesai pertemuan 3

---
+++ profil.php
@@ -0,0 +1,47 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/cicipa.jpg" alt="Hamdanah Norsipa" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2110010099</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Hamdanah Norsipa</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 13 Juni 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0813 5197 8871</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">hamdanahnorsipa136@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://instagram.com/sipa13_" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                                <a href="http://t.me/Sipa13/" target="_blank">
+                                    <img src="img/medsos/telegram.svg" width="30" alt="linkedin">
+                                </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
